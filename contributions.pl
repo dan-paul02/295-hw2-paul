ab39a40c31d77d718d22eb400b17ee575afe83c4
Dan <daniel.paul@hope.edu>
2023-02-06 04:42:18 -0500

    tried to finish contributions.pl author email and commitCount work couldn't get recent or first commit to only print out one commit

--- contributions.pl
+++ contributions.pl
@@ -5,7 +5,7 @@ my $filename = @ARGV[0];
 chomp(my $authorName = `git log --pretty=format:'%an' -- $filename`);
 chomp(my $authorEmail = `git log --pretty=format:'%ae' -- $filename`);
 chomp(my $commitCount = `git shortlog -sn --all -- $filename`);
-chomp(my $firstCommit = `git log --pretty=format:'%ad' --author $authorName | tail -1 -- $filename`);
-chomp(my $recentCount = `git log --pretty=format:'%ad' --author $authorName | head -1 -- $filename`);
+chomp(my $firstCommit = `git log --pretty=format:'%ad' --author $authorName -- $filename -1`);
+chomp(my $recentCount = `git log --pretty=format:'%ad' --author $authorName -- $filename -1`);
 
-print($authorEmail, " ", $commitCount, " ",$firstCommit, " ", $recentCount);
+print($authorEmail, ",", $commitCount, ",",$firstCommit, ",", $recentCount);
